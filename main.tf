d1336296a9e38ef3a2221e4c3358113526e3475d
Ronayrton Rocha <ronayrton16@gmail.com>
2024-11-01 10:44:24 -0300

    Primeiro commit

---
+++ main.tf
@@ -0,0 +1,37 @@
+provider "aws" {
+  region = "us-east-1"  # Altere para sua região preferida
+}
+
+# Tabelas DynamoDB
+resource "aws_dynamodb_table" "cloudmart_products" {
+  name           = "cloudmart-products"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+}
+
+resource "aws_dynamodb_table" "cloudmart_orders" {
+  name           = "cloudmart-orders"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+}
+
+resource "aws_dynamodb_table" "cloudmart_tickets" {
+  name           = "cloudmart-tickets"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+}
